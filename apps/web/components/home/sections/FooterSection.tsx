e8ef242b15e8945a71a9e74c4e02d8c3e85546a2
Farai Chitekedza <developer@shalean.com>
2026-05-31 10:42:43 +0200

    Fix footer client component for GA4 tracking

--- apps/web/components/home/sections/FooterSection.tsx
+++ apps/web/components/home/sections/FooterSection.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { ExternalLink, Mail, MapPin, Phone } from "lucide-react";
 import { locationPageServiceLinks } from "@/lib/seo/capeTownSeoPages";
