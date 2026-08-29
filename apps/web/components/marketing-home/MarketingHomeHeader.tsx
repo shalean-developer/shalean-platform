c223d719b168b2bca07747e5bf2db57ce280f2f8
Farai Chitekedza <developer@shalean.com>
2026-08-29 20:15:18 +0200

    design(home): scope Services hover menu behavior

--- apps/web/components/marketing-home/MarketingHomeHeader.tsx
+++ apps/web/components/marketing-home/MarketingHomeHeader.tsx
@@ -1,8 +1,9 @@
 import { MarketingHomeHeaderBar } from "@/components/marketing-home/MarketingHomeHeaderBar";
+import styles from "@/components/marketing-home/MarketingHomeHeader.module.css";
 
 export function MarketingHomeHeader({ bookingHref }: { bookingHref: string }) {
   return (
-    <header className="sticky top-0 z-40">
+    <header className={`${styles.root} sticky top-0 z-40`}>
       <MarketingHomeHeaderBar bookingHref={bookingHref} />
     </header>
   );
