03a398de28db2bc4e280eed06ab9c9062f8bfafd
Farai Chitekedza <developer@shalean.com>
2026-08-29 19:18:19 +0200

    design(home): remove marketing top bar

--- apps/web/components/marketing-home/MarketingHomeHeader.tsx
+++ apps/web/components/marketing-home/MarketingHomeHeader.tsx
@@ -1,10 +1,8 @@
-import { SiteTopBar } from "@/components/nav/SiteTopBar";
 import { MarketingHomeHeaderBar } from "@/components/marketing-home/MarketingHomeHeaderBar";
 
 export function MarketingHomeHeader({ bookingHref }: { bookingHref: string }) {
   return (
     <header className="sticky top-0 z-40">
-      <SiteTopBar />
       <MarketingHomeHeaderBar bookingHref={bookingHref} />
     </header>
   );
